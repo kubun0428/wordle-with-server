01fa6d7fa90dc4564d8341b0f7f52ea4a3c06d32
Jihee Lee <163643041+kubun0428@users.noreply.github.com>
2024-04-12 18:37:42 +0900

    upload

---
+++ static/js/index.js
@@ -0,0 +1,131 @@
+const keyboardKeys = document.querySelectorAll(
+  ".keyboard-key, .keyboard-key-long"
+);
+
+let attempts = 0;
+let index = 0;
+let timer;
+
+function appStart() {
+  const handleBackspace = () => {
+    if (index !== 0) {
+      index--;
+      const formerBlock = document.querySelector(
+        `.board-column[data-index="${attempts}${index}"]`
+      );
+      formerBlock.innerText = "";
+    }
+  };
+  const displayGameOver = (flag) => {
+    const div = document.createElement("div");
+    if (flag === "found") div.innerText = "Congratulation!";
+    else if (flag === "not_found") div.innerText = "Try Again!";
+    div.style = `
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      color: white;
+      font-size: 35px;
+      z-index: 1000;
+    `;
+    document.body.appendChild(div);
+  };
+
+  const gameOver = (flag) => {
+    window.removeEventListener("keydown", handleKeyDown);
+    displayGameOver(flag);
+    clearInterval(timer);
+  };
+  const nextLine = () => {
+    if (attempts === 5) {
+      gameOver("not_found");
+    }
+    attempts++;
+    index = 0;
+  };
+
+  const handleEnterkey = async () => {
+    let correct_count = 0;
+    const response = await fetch("/answer");
+    const answer = await response.json();
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-column[data-index="${attempts}${i}"]`
+      );
+      const input_word = block.innerText;
+      const answer_word = answer[i];
+      const keyElement = document.querySelector(
+        `.keyboard-key[data-key="${input_word}"], .keyboard-key-long[data-key="${input_word}"]`
+      );
+      if (input_word === answer_word) {
+        correct_count++;
+        block.style.background = "#67B360";
+        if (keyElement) {
+          keyElement.style.backgroundColor = "#67B360";
+          keyElement.setAttribute("correct", "");
+        }
+      } else if (answer.includes(input_word)) {
+        block.style.background = "#D6BE4E";
+        if (keyElement && !keyElement.hasAttribute("correct"))
+          keyElement.style.backgroundColor = "#D6BE4E";
+      } else block.style.background = "grey";
+      block.style.color = "white";
+    }
+
+    if (correct_count === 5) gameOver("found");
+    else nextLine();
+  };
+
+  const startTimer = () => {
+    const start_time = new Date();
+    function setTime() {
+      const current_time = new Date();
+      elapsed_time = new Date(current_time - start_time);
+      const minute = elapsed_time.getMinutes().toString().padStart(2, "0");
+      const second = elapsed_time.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${minute}:${second}`;
+    }
+    timer = setInterval(setTime, 1000);
+  };
+
+  const handleKeyDown = (event) => {
+    const key = event.key;
+    const keyCode = event.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-column[data-index="${attempts}${index}"]`
+    );
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterkey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key.toUpperCase();
+      index++;
+    }
+  };
+
+  const handleClick = (event) => {
+    const key = event.target.getAttribute("data-key");
+
+    if (key === "BACKSPACE") handleBackspace();
+    else if (key === "ENTER") handleEnterkey();
+    else {
+      const tmpEvent = { key: key, keyCode: key.charCodeAt(0) };
+      handleKeyDown(tmpEvent);
+    }
+  };
+  startTimer();
+  window.addEventListener("keydown", handleKeyDown);
+  keyboardKeys.forEach((key) => {
+    key.addEventListener("click", handleClick);
+  });
+}
+
+appStart();
